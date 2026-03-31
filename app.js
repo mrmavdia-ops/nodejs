a403e3065720a9abb6f1f588a9479de02bad1aec
mrmavdia-ops <mrmavdia@gmail.com>
2026-03-31 22:37:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require("path");
 const axios = require("axios");
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true });
 
 // Serve audio files
 app.use("/audio", express.static("public"));
